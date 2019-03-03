ebd1f5c184d8e39a3704df34b9e88f2a07427d06
jingchen-z <z782180503@gmail.com>
2019-03-03 22:07:29 +0800

    Changed coding style

--- src/main/java/seedu/address/logic/command/ClearCommand.java
+++ src/main/java/seedu/address/logic/command/ClearCommand.java
@@ -6,11 +6,9 @@ import seedu.address.logic.command.CommandResult;
  * Clears data in Tasketch.
  */
 public class ClearCommand extends Command {
-    
     public static final String COMMAND_WORD = "clear";
     public static final String COMMAND_ALIAS = "c";
     public static final String MESSAGE_SUCCESS = "Tasketch been cleared!";
-    
     /* @Override */
     public CommandResult execute() {
         //To be further implemented.
