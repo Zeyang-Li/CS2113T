ecf8e4c4371864e7329c85ab373066bc1597a9fc
cheahzk <zhikang94@hotmail.com>
2019-03-08 11:46:30 +0800

    [model]: Morph to Tasketch.

---
+++ src/main/java/seedu/address/model/task/TaskContainsKeywordsPredicate.java
@@ -0,0 +1,32 @@
+package seedu.address.model.task;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.model.task.Task;
+
+/**
+ * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ */
+public class TaskContainsKeywordsPredicate implements Predicate<Task> {
+    private final List<String> keywords;
+
+    public TaskContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Task task) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(task.getName().fullName, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof TaskContainsKeywordsPredicate // instanceof handles nulls
+                && keywords.equals(((TaskContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+}
