6f38417cf3a05b0d5457ae63205578fa630fb662
Zeyang-Li <leonialee990810@gmail.com>
2019-03-17 22:39:45 +0800

    change coding style in clear command

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -1,10 +1,14 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.TaskBook;
 
-import static java.util.Objects.requireNonNull;
+
+
+
 
 /**
  * Clears the Tasketch.
