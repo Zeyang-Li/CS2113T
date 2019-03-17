42362b161ecc2a9a3b567a5070978802977e5888
jingchen-z <z782180503@gmail.com>
2019-03-17 16:12:41 +0800

    Style fixing 1.1

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -2,6 +2,8 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.List;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
@@ -9,12 +11,10 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.task.Task;
 
-import java.util.List;
-
 /**
  * Delete a task.
  */
-public class DeleteCommand extends Command{
+public class DeleteCommand extends Command {
 
     public static final String COMMAND_WORD = "delete";
 
