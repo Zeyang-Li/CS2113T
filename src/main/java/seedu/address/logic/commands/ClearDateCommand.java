a673f40e049f5e033bea3e4c4af35e9b3ac85ab0
Zeyang-Li <leonialee990810@gmail.com>
2019-03-18 20:06:51 +0800

    ClearDateCommand 1.2

--- src/main/java/seedu/address/logic/commands/ClearDateCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearDateCommand.java
@@ -4,10 +4,8 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.lang.String;
 
 import seedu.address.logic.CommandHistory;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.task.Task;
 
@@ -15,7 +13,7 @@ import seedu.address.model.task.Task;
  * Finds and clear all tasks on specific date in Tasketch.
  */
 
-public class ClearDateCommand extends Command{
+public class ClearDateCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
 
@@ -26,29 +24,32 @@ public class ClearDateCommand extends Command{
             + "Example: " + COMMAND_WORD + " 18-03-19";
 
     public static final String MESSAGE_SUCCESS = "clear %1$d tasks on %2$s";
-//    public static final String MESSAGE_FAILURE = "There are no tasks on that day";
-    private String specific_date;
+    private String specificDate;
     private int count;
 
     /**
      * Creates an ClearDateCommand to clear tasks on the specified {@code specific_date}
      */
-    public ClearDateCommand(String specific_date) {
-        this.specific_date = specific_date;
+    public ClearDateCommand(String specificDate) {
+        this.specificDate = specificDate;
     }
 
+    /**
+     * A boolean function that help predicate to select specific tasks.
+     */
     public boolean checkDate(Task task) {
-        final String[] specificDate = specific_date.split("-");
+        final String[] specificDateList = specificDate.split("-");
         final String startDate = task.getStartDate().value;
-        if (specificDate.length == 3) {
-            if (specific_date.equals(startDate)) {
+
+        if (specificDateList.length == 3) {
+            if (specificDate.equals(startDate)) {
                 return true;
             } else {
                 return false;
             }
-        } else if (specificDate.length == 2) {
+        } else if (specificDateList.length == 2) {
             final String[] startDateInString = startDate.split("-");
-            if(specificDate[0].equals(startDateInString[1]) && specificDate[1].equals(startDateInString[2])) {
+            if (specificDateList[0].equals(startDateInString[1]) && specificDateList[1].equals(startDateInString[2])) {
                 return true;
             } else {
                 return false;
@@ -62,11 +63,11 @@ public class ClearDateCommand extends Command{
         requireNonNull(model);
         count = 0;
         List<Task> lastShownList = model.getFilteredTaskList();
-        List<Task> TasksToBeDeleted = new ArrayList<Task>();
-        for(Task task : lastShownList) {
+        List<Task> tasksToBeDeleted = new ArrayList<Task>();
+        for (Task task : lastShownList) {
             if (checkDate(task)) {
                 count++;
-                TasksToBeDeleted.add(task);
+                tasksToBeDeleted.add(task);
             }
         }
 
@@ -74,9 +75,9 @@ public class ClearDateCommand extends Command{
 //            throw new CommandException(MESSAGE_FAILURE);
 //        }
 
-        model.deleteTaskList(TasksToBeDeleted);
+        model.deleteTaskList(tasksToBeDeleted);
         model.commitTaskBook();
-        return new CommandResult(String.format(MESSAGE_SUCCESS, count, specific_date));
+        return new CommandResult(String.format(MESSAGE_SUCCESS, count, specificDate));
     }
 
 }
