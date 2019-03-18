1030a195908f9061ab72ee35bef4dfe7966273f2
Zeyang-Li <leonialee990810@gmail.com>
2019-03-18 10:43:00 +0800

    implement undo and redo

--- src/main/java/seedu/address/logic/commands/RedoCommand.java
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -1,4 +1,34 @@
 package seedu.address.logic.commands;
 
-public class RedoCommand {
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TASKS;
+
+/**
+ * Reverts the {@code model}'s address book to its previously undone state.
+ */
+public class RedoCommand extends Command {
+
+    public static final String COMMAND_WORD = "redo";
+    public static final String COMMAND_ALIAS = "r";
+
+    public static final String MESSAGE_SUCCESS = "Redo success!";
+    public static final String MESSAGE_FAILURE = "No more commands to redo!";
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+
+        if (!model.canRedoTaskBook()) {
+            throw new CommandException(MESSAGE_FAILURE);
+        }
+
+        model.redoTaskBook();
+        model.updateFilteredTaskList(PREDICATE_SHOW_ALL_TASKS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
 }
+
--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -1,4 +1,33 @@
 package seedu.address.logic.commands;
 
-public class UndoCommand {
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TASKS;
+
+/**
+ * Reverts the {@code model}'s address book to its previous state.
+ */
+public class UndoCommand extends Command {
+
+    public static final String COMMAND_WORD = "undo";
+    public static final String COMMAND_ALIAS = "u";
+
+    public static final String MESSAGE_SUCCESS = "Undo success!";
+    public static final String MESSAGE_FAILURE = "No more commands to undo!";
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+
+        if (!model.canUndoTaskBook()) {
+            throw new CommandException(MESSAGE_FAILURE);
+        }
+
+        model.undoTaskBook();
+        model.updateFilteredTaskList(PREDICATE_SHOW_ALL_TASKS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
 }
