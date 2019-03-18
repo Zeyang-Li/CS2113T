b22a825b4fb318a35917630f5f2f91fda4008aed
Zeyang-Li <leonialee990810@gmail.com>
2019-03-18 15:03:31 +0800

    testing: coding style 1.0

--- src/test/java/guitests/guihandles/CommandBoxHandle.java
+++ src/test/java/guitests/guihandles/CommandBoxHandle.java
@@ -5,38 +5,38 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 
 /**
-* A handle to the {@code CommandBox} in the GUI.
-*/
+ * A handle to the {@code CommandBox} in the GUI.
+ */
 public class CommandBoxHandle extends NodeHandle<TextField> {
 
-   public static final String COMMAND_INPUT_FIELD_ID = "#commandTextField";
+    public static final String COMMAND_INPUT_FIELD_ID = "#commandTextField";
 
-   public CommandBoxHandle(TextField commandBoxNode) {
-       super(commandBoxNode);
-   }
+    public CommandBoxHandle(TextField commandBoxNode) {
+        super(commandBoxNode);
+    }
 
-   /**
-    * Returns the text in the command box.
-    */
-   public String getInput() {
-       return getRootNode().getText();
-   }
+    /**
+     * Returns the text in the command box.
+     */
+    public String getInput() {
+        return getRootNode().getText();
+    }
 
-   /**
-    * Enters the given command in the Command Box and presses enter.
-    */
-   public void run(String command) {
-       click();
-       guiRobot.interact(() -> getRootNode().setText(command));
-       guiRobot.pauseForHuman();
+    /**
+     * Enters the given command in the Command Box and presses enter.
+     */
+    public void run(String command) {
+        click();
+        guiRobot.interact(() -> getRootNode().setText(command));
+        guiRobot.pauseForHuman();
 
-       guiRobot.type(KeyCode.ENTER);
-   }
+        guiRobot.type(KeyCode.ENTER);
+    }
 
-   /**
-    * Returns the list of style classes present in the command box.
-    */
-   public ObservableList<String> getStyleClass() {
-       return getRootNode().getStyleClass();
-   }
+    /**
+     * Returns the list of style classes present in the command box.
+     */
+    public ObservableList<String> getStyleClass() {
+        return getRootNode().getStyleClass();
+    }
 }
