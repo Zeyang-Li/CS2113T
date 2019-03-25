49f97a105cccf201c1d8d1fa2d3e660fb9b19e58
BrittonAlone <jinshibo0225@gmail.com>
2019-03-25 19:30:52 +0800

    List commands merge together

--- src/main/java/seedu/address/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -16,13 +16,17 @@ public class ListCommandParser {
      */
     public Command parse(String userInput) throws ParseException {
         String[] arguments = userInput.trim().split("\\s+");
+        
         if (arguments.length == 1 ) {
             if (arguments[0].equals("") || arguments[0].equals("td") || isValidDateFormat(arguments[0])) {
+
                return new ListCommand(arguments);
             } else {
+
                 throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
             }
         } else {
+
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
         }
     }
