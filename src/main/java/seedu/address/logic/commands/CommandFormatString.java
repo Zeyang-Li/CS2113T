8bafbb46c6316dbd54ee6b0172339849dc256401
Wanchunwei <scengka@gmail.com>
2019-04-01 19:36:12 +0800

    coding style

--- src/main/java/seedu/address/logic/commands/CommandFormatString.java
+++ src/main/java/seedu/address/logic/commands/CommandFormatString.java
@@ -9,8 +9,11 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_STARTDATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_STARTTIME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
+/**
+ * Contains format string for input of each command in Command Line Interface (CLI)
+ */
 public class CommandFormatString {
-	public static final String addCommandFormatString = PREFIX_NAME + "task_name  "
+    public static final String ADD_COMMAND_FORMATSTRING = PREFIX_NAME + "task_name  "
             + PREFIX_STARTDATE + "start_date  "
             + PREFIX_STARTTIME + "start_time  "
             + PREFIX_ENDDATE + "end_date  "
@@ -18,14 +21,14 @@ public class CommandFormatString {
             + PREFIX_DESCRIPTION + "content  "
             + PREFIX_CATEGORY + "category  "
             + "[" + PREFIX_TAG + "tag]";
-    public static final String listCommandFormatString = "Date";
-    public static final String editCommandFormatString = PREFIX_NAME + "task name  "
+    public static final String LIST_COMMAND_FORMATSTRING = "Date";
+    public static final String EDIT_COMMAND_FORMATSTRING = PREFIX_NAME + "task name  "
             + PREFIX_STARTDATE + "start_date  "
             + PREFIX_STARTTIME + "start_time  "
             + PREFIX_ENDDATE + "end_date  "
             + PREFIX_ENDTIME + "end_time  "
             + PREFIX_DESCRIPTION + "content  ";
-    public static final String findCommandFormatString = "Keyword";
-    public static final String deleteCommandFormatString = "Index";
-    public static final String clearCommandFormatString = "Date";
+    public static final String FIND_COMMAND_FORMATSTRING = "Keyword";
+    public static final String DELETE_COMMAND_FORMATSTRING = "Index";
+    public static final String CLEAR_COMMAND_FORMATSTRING = "Date";
 }
