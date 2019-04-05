5265452ceaf0221066721d162b656d5c58a97a09
BrittonAlone <jinshibo0225@gmail.com>
2019-04-06 03:14:08 +0800

    List category command added; UG for remind and list updated.

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -23,17 +23,21 @@ public class ListCommand extends Command {
             + "1. no parameters: list        list all tasks\n"
             + "2. td: list td       list all the tasks of today\n"
             + "3. DATE: list 25-03-19    "
-            + "list all the tasks which are before/after 25th March, 2019\n";
+            + "list all the tasks which are before/after 25th March, 2019\n"
+            + "4. category: list a/c/e/r/o";
     public static final String MESSAGE_SUCCESS1 = "Listed all tasks";
     public static final String MESSAGE_SUCCESS2 = "Listed all tasks of today.";
     public static final String MESSAGE_SUCCESS3 = "Listed tasks on %1$s";
+    public static final String MESSAGE_SUCCESS4 = "Listed all tasks of %1$s category";
     public static final String COMMAND_PARAMETERS = "Parameters:\n"
             + "1. no parameters: list        list all tasks\n"
             + "2. td: list td       list all the tasks of today\n"
             + "3. DATE: list 25-03-19    "
-            + "list all the tasks which are before/after 25th March, 2019\n";
+            + "list all the tasks which are before/after 25th March, 2019\n"
+            + "4. category: list a      list all tasks of academic category";
     private String[] arguments;
     private String specifiedDate;
+    private String specifiedCategory;
 
     public ListCommand(String[] arguments) {
         this.arguments = arguments;
@@ -52,6 +56,12 @@ public class ListCommand extends Command {
             model.updateFilteredTaskList(predicate);
             //ObservableList<Task> test = model.getFilteredTaskList();
             return new CommandResult(MESSAGE_SUCCESS2);
+        } else if (isCategory(arguments[0])) {
+
+            specifiedCategory = arguments[0];
+            Predicate<Task> predicate = task -> meetRequirementCategory(task);
+            model.updateFilteredTaskList(predicate);
+            return new CommandResult(String.format(MESSAGE_SUCCESS4, categoryString(specifiedCategory)));
         } else {
 
             specifiedDate = arguments[0];
@@ -76,6 +86,34 @@ public class ListCommand extends Command {
             return true;
         }
         return false;
+    }
+
+    /**
+     * A boolean function used to decide predicate for list category command.
+     */
+    private boolean isCategory(String str) {
+        if (str.equals("a") || str.equals("c") || str.equals("e") || str.equals("r") || str.equals("o")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Return a string that to be printed.
+     */
+    private String categoryString(String str) {
+        if (str.equals("a")) {
+            return "academic";
+        } else if (str.equals("c")) {
+            return "CCA";
+        } else if (str.equals("e")) {
+            return "entertainment";
+        } else if (str.equals("r")) {
+            return "errand";
+        } else {
+            return "other";
+        }
     }
 
     /**
@@ -104,6 +142,18 @@ public class ListCommand extends Command {
             }
         }
         return false;
+    }
+
+    /**
+     * A boolean function used to decide predicate for list category command.
+     */
+    private boolean meetRequirementCategory(Task task) {
+        String taskCategory = task.getCategories().value;
+        if (taskCategory.equals(specifiedCategory)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
