4893db9a9a1fa1a5d51fa8abf5a064998de6ddde
cheahzk <zhikang94@hotmail.com>
2019-04-05 10:52:31 +0800

    Remove duplicate codes for date and time checking.

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -9,6 +9,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "The task index provided is invalid";
     public static final String MESSAGE_TASK_LISTED_OVERVIEW = "%1$d tasks listed!";
-    public static final String MESSAGE_DAY_LISTED_OVERVIEW = "%1$d days listed!";
+    public static final String MESSAGE_DATE_CONSTRAINTS = "Start Date must be before End Date!";
+    public static final String MESSAGE_TIME_CONSTRAINTS = "Start Time must be before End Time!";
 
 }
