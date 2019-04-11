a60a86bacd01b9b7e9c86599b60b01cc872689fa
cheahzk <zhikang94@hotmail.com>
2019-04-11 17:37:31 +0800

    Travis checkstyle fix 1.5

--- src/main/java/seedu/address/model/ReadOnlyTaskBook.java
+++ src/main/java/seedu/address/model/ReadOnlyTaskBook.java
@@ -1,10 +1,7 @@
 package seedu.address.model;
 
-import java.util.Map;
-
 import javafx.beans.Observable;
 import javafx.collections.ObservableList;
-import seedu.address.model.day.Date;
 import seedu.address.model.day.Day;
 import seedu.address.model.task.Task;
 
