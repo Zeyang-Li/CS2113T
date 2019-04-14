7b79acbae046eca9c9d525cee14bb060df25c870
Wanchunwei <scengka@gmail.com>
2019-04-14 21:49:14 +0800

    Update PPP

--- src/test/java/seedu/address/logic/commands/ExitCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExitCommandTest.java
@@ -2,21 +2,35 @@ package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.ExitCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT;
+import static seedu.address.testutil.TypicalTasks.getTypicalTaskBook;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.CommandHistory;
+import seedu.address.model.AccountList;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.account.Username;
 
 public class ExitCommandTest {
     private Model model = new ModelManager();
     private Model expectedModel = new ModelManager();
     private CommandHistory commandHistory = new CommandHistory();
+
+    @Before
+    public void setUp() {
+        model = new ModelManager(getTypicalTaskBook(), new UserPrefs(), new AccountList());
+        expectedModel = new ModelManager(model.getTaskBook(), new UserPrefs(), new AccountList());
+        Username admin = new Username("admin");
+        model.setLoggedInUser(admin);
+    }
 
     @Test
     public void execute_exit_success() throws IOException, IllegalValueException, DataConversionException {
